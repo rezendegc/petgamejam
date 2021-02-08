a29f28721161d8d97e37d786ea71322137162c5e
Gabriel Rezende <gabrielcdrezende@gmail.com>
2021-02-08 01:36:01 -0300

    new build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "ee5d43403c340f90069123383d263ceb",
-"/": "ee5d43403c340f90069123383d263ceb",
+  "index.html": "38a6d424c4220a9d7df9f7d26b5ad1e5",
+"/": "38a6d424c4220a9d7df9f7d26b5ad1e5",
 "manifest.json": "c76cda6f8516ff9d0de26cdac7869f01",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
